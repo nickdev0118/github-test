862e1d03ad3129d1eae501a74442282664bb1ee4
nickdev0118 <nickdev0118@gmail.com>
2024-03-01 01:55:23 +0700

    first commit

---
+++ components/home/photo-booth.tsx
@@ -0,0 +1,201 @@
+import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import { Download } from "lucide-react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { useEffect, useMemo, useState } from "react";
+import { LoadingCircle } from "../shared/icons";
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+function forceDownload(blobUrl: string, filename: string) {
+  let a: any = document.createElement("a");
+  a.download = filename;
+  a.href = blobUrl;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
+
+export default function PhotoBooth({
+  input,
+  blurDataURL,
+  output,
+  failed,
+}: {
+  input: string;
+  blurDataURL: string;
+  output: string | null;
+  failed?: boolean;
+}) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [state, setState] = useState("output");
+  const direction = useMemo(() => (state === "output" ? 1 : -1), [state]);
+  const [downloading, setDownloading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (loading) {
+        setShowForm(true);
+      }
+    }, 5000);
+  }, [loading]);
+
+  useEffect(() => {
+    if (output) {
+      setLoading(false);
+    }
+  }, [output]);
+
+  return (
+    <motion.div
+      className="group relative mx-auto mt-10 h-[350px] w-full overflow-hidden rounded-2xl border border-gray-200 sm:h-[600px] sm:w-[600px]"
+      variants={FADE_DOWN_ANIMATION_VARIANTS}
+    >
+      <button
+        onClick={() => setState(state === "output" ? "input" : "output")}
+        className="absolute left-5 top-5 z-10 rounded-full border border-gray-200 bg-white px-4 py-2 shadow-sm transition-all hover:scale-105 active:scale-95"
+      >
+        <p className="text-sm font-semibold text-gray-500">
+          {state === "output" ? "View original" : "View result"}
+        </p>
+      </button>
+      {/* 
+        only show the download button if:
+          - it's on a page with an id (i.e. not the demo page) 
+          - there's an output
+          - we're in the output tab
+      */}
+      {id && output && state === "output" && !failed && (
+        <button
+          onClick={() => {
+            setDownloading(true);
+            fetch(output, {
+              headers: new Headers({
+                Origin: location.origin,
+              }),
+              mode: "cors",
+            })
+              .then((response) => response.blob())
+              .then((blob) => {
+                let blobUrl = window.URL.createObjectURL(blob);
+                forceDownload(
+                  blobUrl,
+                  `${id || "demo"}.${state === "output" ? "gif" : ""}`,
+                );
+                setDownloading(false);
+              })
+              .catch((e) => console.error(e));
+          }}
+          className="absolute right-5 top-5 z-10 flex h-9 w-9 items-center justify-center rounded-full border border-gray-200 bg-white shadow-sm transition-all hover:scale-105 active:scale-95"
+        >
+          {downloading ? (
+            <LoadingCircle />
+          ) : (
+            <Download className="h-5 w-5 text-gray-500" />
+          )}
+        </button>
+      )}
+      <MotionConfig
+        transition={{
+          x: { type: "spring", stiffness: 300, damping: 30 },
+          opacity: { duration: 0.2 },
+        }}
+      >
+        <AnimatePresence initial={false} custom={direction}>
+          {state === "output" ? (
+            <motion.div
+              key={output}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              className="absolute h-full w-full"
+            >
+              {failed && (
+                <div className="z-10 flex h-full w-full flex-col items-center bg-white pt-[140px] sm:pt-[280px]">
+                  <p className="text-sm text-red-600">
+                    Failed to run - could not find face in image. Try another!
+                  </p>
+                </div>
+              )}
+              {loading && (
+                <div className="z-10 flex h-full w-full flex-col items-center bg-white pt-[140px] sm:pt-[280px]">
+                  <LoadingCircle />
+                  {id && showForm && (
+                    <motion.div
+                      className="my-4 flex flex-col items-center space-y-4"
+                      initial="hidden"
+                      whileInView="show"
+                      animate="show"
+                      viewport={{ once: true }}
+                    >
+                      <motion.p
+                        className="text-sm text-gray-500"
+                        variants={FADE_DOWN_ANIMATION_VARIANTS}
+                      >
+                        This can take anywhere between 2-3 minutes to run.
+                      </motion.p>
+                    </motion.div>
+                  )}
+                </div>
+              )}
+              {output && (
+                <Image
+                  alt="output image"
+                  src={output}
+                  width={1280}
+                  height={1280}
+                  className="h-full object-cover"
+                  onLoadStart={() => setLoading(true)}
+                  onLoadingComplete={() => setLoading(false)}
+                />
+              )}
+            </motion.div>
+          ) : (
+            <motion.div
+              key={input}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              className="absolute h-full w-full"
+            >
+              <Image
+                alt="original image"
+                src={input}
+                className="object-cover"
+                placeholder="blur"
+                blurDataURL={blurDataURL}
+                fill
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </MotionConfig>
+    </motion.div>
+  );
+}
---
+++ components/home/upload-modal.tsx
@@ -0,0 +1,226 @@
+import Modal from "@/components/shared/modal";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useMemo,
+  ChangeEvent,
+} from "react";
+import Image from "next/image";
+import { UploadCloud } from "lucide-react";
+import { LoadingDots } from "@/components/shared/icons";
+import { useRouter } from "next/router";
+
+const UploadModal = ({
+  showUploadModal,
+  setShowUploadModal,
+}: {
+  showUploadModal: boolean;
+  setShowUploadModal: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const router = useRouter();
+  const [data, setData] = useState<{
+    image: string | null;
+  }>({
+    image: null,
+  });
+  const [fileSizeTooBig, setFileSizeTooBig] = useState(false);
+
+  const [dragActive, setDragActive] = useState(false);
+
+  const onChangePicture = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setFileSizeTooBig(false);
+      const file = event.currentTarget.files && event.currentTarget.files[0];
+      if (file) {
+        if (file.size / 1024 / 1024 > 5) {
+          setFileSizeTooBig(true);
+        } else {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            setData((prev) => ({ ...prev, image: e.target?.result as string }));
+          };
+          reader.readAsDataURL(file);
+        }
+      }
+    },
+    [setData],
+  );
+
+  const [saving, setSaving] = useState(false);
+
+  const saveDisabled = useMemo(() => {
+    return !data.image || saving;
+  }, [data.image, saving]);
+
+  return (
+    <Modal showModal={showUploadModal} setShowModal={setShowUploadModal}>
+      <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
+          <a href="https://extrapolate.app">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              className="h-10 w-10 rounded-full"
+              width={20}
+              height={20}
+            />
+          </a>
+          <h3 className="font-display text-2xl font-bold">Upload Photo</h3>
+          <p className="text-sm text-gray-500">
+            Your photos will be automatically deleted after 24 hours.
+          </p>
+        </div>
+
+        <form
+          className="grid gap-6 bg-gray-50 px-4 py-8 md:px-16"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setSaving(true);
+            fetch("/api/upload", {
+              method: "POST",
+              body: JSON.stringify(data),
+            }).then(async (res) => {
+              if (res.status === 200) {
+                const { key } = await res.json();
+                router.push(`/p/${key}`);
+              } else {
+                setSaving(false);
+                alert("Something went wrong. Please try again later.");
+              }
+            });
+          }}
+        >
+          <div>
+            <div className="flex items-center justify-between">
+              <p className="block text-sm font-medium text-gray-700">Photo</p>
+              {fileSizeTooBig && (
+                <p className="text-sm text-red-500">
+                  File size too big (max 5MB)
+                </p>
+              )}
+            </div>
+            <label
+              htmlFor="image-upload"
+              className="group relative mt-2 flex h-72 cursor-pointer flex-col items-center justify-center rounded-md border border-gray-300 bg-white shadow-sm transition-all hover:bg-gray-50"
+            >
+              <div
+                className="absolute z-[5] h-full w-full rounded-md"
+                onDragOver={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setDragActive(true);
+                }}
+                onDragEnter={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setDragActive(true);
+                }}
+                onDragLeave={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setDragActive(false);
+                }}
+                onDrop={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setDragActive(false);
+                  setFileSizeTooBig(false);
+                  const file = e.dataTransfer.files && e.dataTransfer.files[0];
+                  if (file) {
+                    if (file.size / 1024 / 1024 > 5) {
+                      setFileSizeTooBig(true);
+                    } else {
+                      const reader = new FileReader();
+                      reader.onload = (e) => {
+                        setData((prev) => ({
+                          ...prev,
+                          image: e.target?.result as string,
+                        }));
+                      };
+                      reader.readAsDataURL(file);
+                    }
+                  }
+                }}
+              />
+              <div
+                className={`${
+                  dragActive ? "border-2 border-black" : ""
+                } absolute z-[3] flex h-full w-full flex-col items-center justify-center rounded-md px-10 transition-all ${
+                  data.image
+                    ? "bg-white/80 opacity-0 hover:opacity-100 hover:backdrop-blur-md"
+                    : "bg-white opacity-100 hover:bg-gray-50"
+                }`}
+              >
+                <UploadCloud
+                  className={`${
+                    dragActive ? "scale-110" : "scale-100"
+                  } h-7 w-7 text-gray-500 transition-all duration-75 group-hover:scale-110 group-active:scale-95`}
+                />
+                <p className="mt-2 text-center text-sm text-gray-500">
+                  Drag and drop or click to upload.
+                </p>
+                <p className="mt-2 text-center text-sm text-gray-500">
+                  Recommended: 1:1 square ratio, with a clear view of your face
+                </p>
+                <span className="sr-only">Photo upload</span>
+              </div>
+              {data.image && (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  src={data.image}
+                  alt="Preview"
+                  className="h-full w-full rounded-md object-cover"
+                />
+              )}
+            </label>
+            <div className="mt-1 flex rounded-md shadow-sm">
+              <input
+                id="image-upload"
+                name="image"
+                type="file"
+                accept="image/*"
+                className="sr-only"
+                onChange={onChangePicture}
+              />
+            </div>
+          </div>
+
+          <button
+            disabled={saveDisabled}
+            className={`${
+              saveDisabled
+                ? "cursor-not-allowed border-gray-200 bg-gray-100 text-gray-400"
+                : "border-black bg-black text-white hover:bg-white hover:text-black"
+            } flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none`}
+          >
+            {saving ? (
+              <LoadingDots color="#808080" />
+            ) : (
+              <p className="text-sm">Confirm upload</p>
+            )}
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export function useUploadModal() {
+  const [showUploadModal, setShowUploadModal] = useState(false);
+
+  const UploadModalCallback = useCallback(() => {
+    return (
+      <UploadModal
+        showUploadModal={showUploadModal}
+        setShowUploadModal={setShowUploadModal}
+      />
+    );
+  }, [showUploadModal, setShowUploadModal]);
+
+  return useMemo(
+    () => ({ setShowUploadModal, UploadModal: UploadModalCallback }),
+    [setShowUploadModal, UploadModalCallback],
+  );
+}
